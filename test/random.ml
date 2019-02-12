9e63d8594d480985a20a0982fe4f34d8bd680f84
r6eve <r6eve179f@gmail.com>
2019-02-13 06:07:07 +0900

    .

---
+++ test/random.ml
@@ -0,0 +1,13 @@
+(****************************************************************)
+(*                                                              *)
+(*           Copyright r6eve 2019 -                             *)
+(*  Distributed under the Boost Software License, Version 1.0.  *)
+(*     (See accompanying file LICENSE_1_0.txt or copy at        *)
+(*           https://www.boost.org/LICENSE_1_0.txt)             *)
+(*                                                              *)
+(****************************************************************)
+
+open Orandom.Random
+
+let () =
+  assert ("junk" = Random.init ())
