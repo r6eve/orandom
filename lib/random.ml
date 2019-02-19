be2b20341b2797f5725d0bc0eb5d222d166ad363
r6eve <r6eve179f@gmail.com>
2019-02-19 14:42:09 +0900

    .

--- lib/random.ml
+++ lib/random.ml
@@ -8,20 +8,24 @@
 (****************************************************************)
 
 module Random = struct
-  let seed = ref 0
+  let seed = ref Int64.zero
 
-  let next_next_gaussian_p = ref false
+  let _next_next_gaussian_p = ref false
 
-  let next_gaussian_p = ref false
+  let _next_gaussian_p = ref false
 
   let set_seed s =
-    seed := (land) (1 lsl 48 - 1) @@ (lor) 0x5DEECE66D @@ s
+    seed := Int64.logand (Int64.of_int (1 lsl 48 - 1)) @@ Int64.logor 0x5DEECE66DL @@ Int64.of_int s
 
   let init () =
     let milli_time = truncate @@ ( *. ) 1000. @@ Unix.gettimeofday () in
     set_seed milli_time
 
-  let next_boolean () = assert false
+  let next bits =
+    seed := Int64.logand (Int64.of_int (1 lsl 48 - 1)) @@ Int64.add 0xBL @@ Int64.mul 0x5DEECE66DL @@ !seed;
+    Int64.shift_right_logical !seed (48 - bits)
+
+  let next_boolean () = next(1) <> Int64.zero
 
   let next_int () = assert false
 
